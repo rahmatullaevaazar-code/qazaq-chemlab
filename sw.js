1a40d86189b2b6ff307538f2a14a8fa44666c3b0
rahmatullaevaazar-code <rahmatullaevaazar@gmail.com>
2026-09-24 10:37:56 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,37 @@
-const CACHE="qazaq-chemlab-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
+const CACHE = "qazaq-chemlab-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+
+  if (
+    e.request.method !== "GET" ||
+    (url.protocol !== "http:" && url.protocol !== "https:")
+  ) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
